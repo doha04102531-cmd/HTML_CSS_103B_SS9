a391462bb4c6c701b6daff980da096096ea87164
doha04102531-cmd <doha04102531@gmail.com>
2026-02-25 14:19:23 +0700

    Update baiktra.js

--- baiktra.js
+++ baiktra.js
@@ -1,9 +1,17 @@
 let orders = ["Đơn hàng A", "Đơn hàng B", "Đơn hàng C", "Đơn hàng D", "Đơn hàng E"];
 let revenues = [1500, 2800, 1200, -500, 3200];
 
+
 for (let i = 0; i < orders.length; i++) {
     const allString = orders[i].concat(revenues[i])
-    console.log(`${orders[i]} mang về ${revenues[i]}`);
-}
+    console.log(`"${orders[i]} mang về ${revenues[i]}",`);
 
+}
+let sum = 0;
+for (let i = 0; i < revenues.length; i++) {
+    if (revenues[i] > 0) {
+        sum += revenues[i];
+    } 
+}
+console.log(sum);
 
